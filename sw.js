feb2f9ed7e1ab1a21cdd3bb87e17d8879b0255d2
dwikyls <kreuzvernicterlanze@gmail.com>
2020-08-16 08:30:27 +0700

    nyobain sw part 2

--- sw.js
+++ sw.js
@@ -1,41 +1,25 @@
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-      caches.open('v1').then(function(cache) {
-        return cache.addAll([
-          '/',
-          'index.html',
-          '/assets/css/style.css',
-          '/app.js',
-          '/assets/img/1.png',
-          '/assets/img/2.png',
-          '/assets/img/3.png',
-          '/assets/img/165_a.png',
-          '/assets/img/a.png'
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', function(event) {
-    event.respondWith(caches.match(event.request).then(function(response) {
-      // caches.match() always resolves
-      // but in case of success response will have value
-      if (response !== undefined) {
-        return response;
-      } else {
-        return fetch(event.request).then(function (response) {
-          // response may be used only once
-          // we need to save clone to put one copy in cache
-          // and serve second one
-          let responseClone = response.clone();
-          
-          caches.open('v1').then(function (cache) {
-            cache.put(event.request, responseClone);
-          });
-          return response;
-        }).catch(function () {
-          return caches.match('/assets/img/165_a.png');
-        });
-      }
-    }));
-  });
+var cacheName = 'hello-world-page';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/assets/css/main.css'
+];
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+self.addEventListener('activate',  event => {
+  event.waitUntil(self.clients.claim());
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request, {ignoreSearch:true}).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
